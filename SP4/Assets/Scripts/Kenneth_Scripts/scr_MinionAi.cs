56450dab5d6e7e47f4eab35d64cf91cdb1399dee
Qumiic <Demourblitz@gmail.com>
2024-02-15 11:47:04 +0800

    test

--- SP4/Assets/Scripts/Kenneth_Scripts/scr_MinionAi.cs
+++ SP4/Assets/Scripts/Kenneth_Scripts/scr_MinionAi.cs
@@ -1,5 +1,6 @@
 using UnityEngine.AI;
 using UnityEngine;
+using System.Collections;
 
 public class scr_MinionAi : MonoBehaviour
 {
@@ -8,8 +9,13 @@ public class scr_MinionAi : MonoBehaviour
     public LayerMask whatIsGround, whatIsPlayer;
     private Animator animator;
 
-    [Header("Stats")]
-    public float health = 65;
+    //[Header("Stats")]
+    //public float health = 65;
+
+    // Idle
+    [Header("Idle")]
+    public float idleDuration = 5f;
+    private float idleTimer = 0f;
 
     // Patrolling
     [Header("Patrol")]
@@ -22,6 +28,9 @@ public class scr_MinionAi : MonoBehaviour
     public float timeBetweenAttacks;
     bool alreadyAttacked;
 
+    // Dead
+    public bool isDead;
+
     // Range
     [Header("Range")]
     public float sightRange, attackRange;
@@ -30,20 +39,26 @@ public class scr_MinionAi : MonoBehaviour
     // States
     private enum MinionState
     {
+        Idle,
         Patrol,
         Chase,
         Attack,
         Dead,
     }
 
-    private MinionState currentState = MinionState.Patrol;
+    private MinionState currentState = MinionState.Idle;
 
     private void Awake()
     {
-        target = GameObject.Find("Player").transform;
-        //target = PlayerManager.instance.player.transform;
+        //target = GameObject.Find("Player").transform;
+        target = GameObject.FindGameObjectWithTag("Player").transform;
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        isDead = false;
     }
 
     private void Update()
@@ -54,6 +69,10 @@ public class scr_MinionAi : MonoBehaviour
 
         switch (currentState)
         {
+            case MinionState.Idle:
+                Idle();
+                break;
+
             case MinionState.Patrol:
                 if (playerInSightRange && !playerInAttackRange)
                 {
@@ -106,15 +125,30 @@ public class scr_MinionAi : MonoBehaviour
                 break;
 
             case MinionState.Dead:
-                // Handle Dead state if needed
+                Death();
+
                 break;
         }
 
     }
 
+    private void Idle()
+    {
+        animator.SetBool("isPatrolling", false);
+
+        idleTimer += Time.deltaTime;
+        if (idleTimer >= idleDuration)
+        {
+            idleTimer = 0f;
+            currentState = MinionState.Patrol;
+        }
+    }
+
     private void Patroling()
     {
+        animator.SetBool("isPatrolling", true);
         animator.SetBool("isChasing", false);
+        
         if (!walkPointSet) SearchWalkPoint();
 
         if (walkPointSet)
@@ -144,6 +178,7 @@ public class scr_MinionAi : MonoBehaviour
 
     private void ChasePlayer()
     {
+        
         animator.SetBool("isChasing", true);
         agent.SetDestination(target.position);
         agent.speed = 5;
@@ -163,23 +198,40 @@ public class scr_MinionAi : MonoBehaviour
             
             ///
             alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+            animator.SetBool("isAttacking", true);
+            StartCoroutine(ResetAttackAfterAnimation());
         }
     }
 
-    private void ResetAttack()
+    private IEnumerator ResetAttackAfterAnimation()
+    {
+        // Wait for the duration of the attack animation
+        yield return new WaitForSeconds(timeBetweenAttacks);
+
+        // Reset attack state and animation
+        ResetAttackState();
+    }
+
+    public void ResetAttackState()
     {
         alreadyAttacked = false;
+        animator.SetBool("isAttacking", false);
     }
 
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
+    //public void TakeDamage(int damage)
+    //{
+    //    health -= damage;
 
-        if (health <= 0f)
-        {
-            Invoke(nameof(DestroyEnemy), 0.5f);
-        }
+    //    if (health <= 0f)
+    //    {
+    //        Invoke(nameof(DestroyEnemy), 0.5f);
+    //    }
+    //}
+
+    private void Death()
+    {
+        isDead = true;
+        animator.SetBool("isDead", true);
     }
 
     private void DestroyEnemy()
